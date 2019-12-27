a31b0c8487057fdbcb782ff646dcb41d753ed5e6
FakeCode <iamnarutu@hotmail.com>
2019-12-27 17:18:10 -0600

    Designed the free space management  and edit the ListCreate,  ListCount, ListAdd

--- list.h
+++ list.h
@@ -3,3 +3,75 @@
  remake the assignment list for self usage and practice
 */
 
+/*
+ Header file for list library
+ The list functions has been devided to 3 parts:
+  -Adders: create, add new node/content to the list
+  -Movers: move the cursor, the nodes of the list
+  -Removers: remove the designated target
+*/
+
+// Argument of LIST
+// Library shall have no dynamic memory allocation on a per-list or per-node
+extern int MAX_LIST;
+extern int MAX_NODE;
+extern int LIST_COUNT;
+extern int LIST_NODE;
+
+// define the standard doubly linked list structure
+typedef struct NODE
+{
+  struct NODE *prev; // point to the previous node
+  void *data; // pointer, hold the data address
+  struct NODE *next; // point to the next node
+} NODE;
+
+typedef struct LIST
+{
+  struct NODE *head; // hold the front of list
+  struct NODE *tail; // hold the tail of list for convenience
+  struct NODE *cursor; // cursor, the node that currenetly operate
+  int count;  // number of items/nodes of the list
+} LIST;
+
+
+typedef struct LIST* list;
+typedef void* item;
+
+
+// Adder functions
+LIST * ListCreate();
+
+int ListCount(list);
+
+int ListAdd(list, item);
+
+int ListInsert(list, item);
+
+int ListAppend(list, item);
+
+int ListPrepend(list, item);
+
+
+// Movers functions
+void *ListFirst(list);
+
+void *ListLast(list);
+
+void *ListNext(list);
+
+void *ListPrev(list);
+
+void *ListCurr(list);
+
+void *ListSearch(list, void* comparator, void* comparisonArg);
+
+
+// Removers functions
+void *ListRemove(list);
+
+void ListConcat(list, list);
+
+void ListFree(list, item);
+
+void *ListTrim(list);
--- list_adders.c
+++ list_adders.c
@@ -2,3 +2,159 @@
  Fake code, Yue Weng, Dec23, 2019
  remake the assignment list for self usage and practice
 */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include "list.h"
+
+#define MAX_LIST 8
+#define MAX_NODE 16
+
+// Count the total assigned objects
+int LIST_COUNT = 0;
+int NODE_COUNT = 0;
+
+// For manage the freed NODES & LISTS in the pool
+int LIST_FREE_NUM = 0;
+int NODE_FREE_NUM = 0;
+
+// Define the pool size, static memmory
+NODE NODE_MEM[MAX_NODE];
+LIST LIST_MEM[MAX_LIST];
+
+// Anchor of freed objects
+NODE* FreeNODES;
+NODE* FreeLISTS;
+
+
+// Adder functions
+
+/*
+ * Function: Assign a new list from the pool
+ * Input: None
+ * Output:
+ *  if success, return back the reference of the new list.
+ *  if fail, return NULL;
+ */
+LIST * ListCreate()
+{
+  LIST* tempList;
+  // there is free space for new list
+  if (LIST_COUNT < MAX_LIST)
+  {
+    // try find a freed list from Hook
+    if (LIST_FREE_NUM> 0)
+    {
+      LIST_COUNT++;
+      LIST_FREE_NUM--;
+      tempList = (LIST*) FreeLISTS->data;
+      // unhook the head & update the anchor
+      FreeLISTS = FreeLISTS->next;
+      return tempList;
+    }
+    else{
+      // return the address of next unused postion of the list pool
+      // then, increment COUNT number
+      return &LIST_MEM[LIST_COUNT++];
+    }
+  }else{
+    // no free list exist
+    return NULL;
+  }
+}
+
+
+/*
+ * Function: Return back the item amount of the argument list pointer
+ * Input: list, a LIST pointer
+ * Output: amount of item in the list, integer.
+ */
+int ListCount(LIST* list)
+{
+  return list->count;
+}
+
+
+/*
+ * Function: Add the new item to the list
+ * Input:
+ *  list, LIST pointer of the target list
+ *  item, void pointer of the item which will be added to the list
+ * Output:
+ *  if success, return 0.
+ *  if fail, return -1. <basically, that means no free space for use>
+ */
+int ListAdd(LIST* list, void* item)
+{
+  NODE* tempNode;
+  int free_index;
+  // find a free node from pool
+  if (NODE_COUNT<MAX_NODE)
+  {
+    // find a node
+    // always try to use the freed node first
+    if (NODE_FREE_NUM>0)
+    {
+      NODE_COUNT++;
+      NODE_FREE_NUM--;
+      tempNode =(NODE*)FreeNODES->data;
+      FreeNODES = FreeNODES->next;
+      tempNode->data = item;
+    }else{
+      tempNode = &NODE_MEM[NODE_COUNT++];
+    }
+
+    // assign the node
+    if (list->count == 0)
+    {
+      list->head = tempNode;
+      list->tail = tempNode;
+      list->count++;
+      list->cursor = tempNode;
+    }else
+    {
+      tempNode->prev = list->cursor;
+      tempNode->next = list->cursor->next;
+      list->cursor->next = tempNode;
+      list->cursor = tempNode;
+      list->count++;
+      // if the new node become the new tail
+      if(list->cursor->next == NULL)
+      {
+        list->tail = tempNode;
+      }  
+    }
+    return 0;
+  }
+  else
+  {
+    // No node can be assigned
+    return -1;
+  }
+}
+
+int ListInsert(LIST* list, void* item)
+{
+  return 0;
+}
+
+int ListAppend(LIST* list, void* item)
+{
+  return 0;
+}
+
+int ListPrepend(LIST* list, void* item)
+{
+  return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
